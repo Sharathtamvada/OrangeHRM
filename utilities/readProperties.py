a88be5eb6db07840d84cb5ded0fa11ad879bdd23
Sharath Tamvada <65681088+Sharathrao98@users.noreply.github.com>
2024-11-23 12:19:01 +0530

    made changes to readProperties.py

--- utilities/readProperties.py
+++ utilities/readProperties.py
@@ -2,7 +2,7 @@ import configparser
 import os
 
 config = configparser.RawConfigParser()
-config.read(os.path.abspath(os.curdir)+'\\configurations\\config.ini')
+config.read(os.path.abspath(os.curdir)+'/configurations/config.ini')
 
 class ReadConfig():
     @staticmethod
